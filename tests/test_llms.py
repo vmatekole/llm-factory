5fab87aaf8741dfa15afa1614161fd798e7e3e4e
Victor Matekole <victor@matekole.com>
2023-10-13 12:58:00 +0200

    remove test

--- tests/test_llms.py
+++ tests/test_llms.py
@@ -8,7 +8,6 @@ from langchain.llms.openai import OpenAIChat
 from llm_maker import LLAMA_MODEL_PATH
 from llm_maker.base_llm import Configuration, LLMModel, LLMService
 from llm_maker.llms import GPT4AllModel, LLMFactory, OpenAIModel
-from main import configure_llm
 
 
 class TestOpenAIModel(unittest.TestCase):
@@ -30,11 +29,12 @@ class TestGPT4AllModel(unittest.TestCase):
 
 
 class TestLLMFactory(unittest.TestCase):
-    def configure_llm(binder):
+    @staticmethod
+    def _configure_llm(binder):
         config = Configuration(
             provider='GPT4All',
             temperature=0,
-            model_filepath=LLAMA_MODELPATH,
+            model_filepath=LLAMA_MODEL_PATH,
             verbose=True,
         )
         binder.bind(Configuration, to=config, scope=singleton)
@@ -50,5 +50,5 @@ class TestLLMFactory(unittest.TestCase):
         self.assertIsInstance(model._provider, OpenAIChat)
 
     def test_singleton(self) -> None:
-        injector: Injector = Injector([configure_llm, LLMFactory()])
+        injector: Injector = Injector([TestLLMFactory._configure_llm, LLMFactory()])
         injector.get(LLMService) is injector.get(LLMService)
